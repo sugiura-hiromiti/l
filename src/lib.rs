fdd74054aa08b8416768393fa2ea8492f4ea5bed
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-08 18:31:13 +0900

    fix typo

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,6 @@ mod tests {
 		sh_cmd!("ls", ["-a"]);
 		sh_cmd!("ls", ["-a", "--color=auto"]);
 		sh_cmd!("ls", ["-l", "-a", "--color=auto"]);
-		//this should cause error sh_cmd!("ls", );
 	}
 
 	#[test]
