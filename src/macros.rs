5ce78ffd4490a43e92c967e76647cad1e3e150db
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-30 11:32:06 +0900

    macro(sh_cmd) return `Result`

--- src/macros.rs
+++ src/macros.rs
@@ -23,7 +23,7 @@ macro_rules! cin {
 /// - $cmd: command itself
 /// - $args: iterator of arguments to be passed to `$cmd`
 /// # Return
-/// return value is ()
+/// `sh_cmd` returns io::Result<Output>
 /// # Explanation
 /// Execute shell command. Then show result.
 macro_rules! sh_cmd {
@@ -31,13 +31,7 @@ macro_rules! sh_cmd {
 		let mut cmd = std::process::Command::new($cmd,);
 		cmd.args($args,);
 
-		//show execution result
-		let output = cmd.output().unwrap();
-		println!("\n |{}: {}\n", $cmd, output.status,);
-		{
-			use std::io::Write;
-			std::io::stdout().write(&output.stdout,).unwrap();
-			std::io::stderr().write(&output.stderr,).unwrap();
-		};
+		//return execution result
+		cmd.output()
 	};
 }
